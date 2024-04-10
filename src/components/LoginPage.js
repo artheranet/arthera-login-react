e5e5e45fab52d16c90b2a8c94dcbf07e02ac5303
flaviusburca <flaviusburca@users.noreply.github.com>
2024-04-10 12:50:58 +0300

    Updates for the new sdk

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,26 +2,39 @@ import React, { useCallback, useState } from 'react';
 import User from './User';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { decodeJwt } from 'jose';
-import Login from '../services/login';
+import { ArtheraLogin } from '@artherachain/mpc-sdk';
 
 const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 
 const LoginPage = () => {
   const [socialProvider, setSocialProvider] = useState('');
   const [profile, setProfile] = useState(null);
-  const login = new Login();
+  const [txReceipt, setTxReceipt] = useState(null);
+  const [sdk, setSdk] = useState(new ArtheraLogin(10243));
 
   const onLogoutSuccess = useCallback(() => {
     setProfile(null);
     setSocialProvider('');
-    login.logout();
+    sdk.logout();
     // alert('logout success');
   }, []);
+
+  const onSendTx = useCallback(async () => {
+    try {
+      const receipt = await sdk.getSigner().sendTransaction({
+        to: sdk.getWallet(),
+        value: "100000000000000000"
+      });
+      setTxReceipt(receipt);
+    } catch (e) {
+      alert(e);
+    }
+  });
 
   return (
     <>
       {socialProvider && profile ? (
-        <User provider={socialProvider} profile={profile} onLogout={onLogoutSuccess} />
+        <User provider={socialProvider} profile={profile} onLogout={onLogoutSuccess} onSendTx={onSendTx} txReceipt={txReceipt}/>
       ) : (
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
           <div style={{ width: 'fit-content' }}>
@@ -31,8 +44,8 @@ const LoginPage = () => {
                 onSuccess={async(response) => {
                   if (response.credential) {
                     const decoded = decodeJwt(response.credential);
-                    await login.loginWithGoogle(googleClientId, response.credential, decoded.sub);
-                    const wallet = login.getWallet();
+                    await sdk.loginWithGoogleToken(googleClientId, decoded.sub, response.credential);
+                    const wallet = sdk.getWallet();
                     setProfile({
                       name: decoded.name,
                       email: decoded.email,
--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const User = ({ provider, profile, onLogout }) => {
+const User = ({ provider, profile, onLogout, onSendTx, txReceipt }) => {
   return (
     <div className="user-info">
       <h2>User Information</h2>
@@ -9,6 +9,9 @@ const User = ({ provider, profile, onLogout }) => {
       {profile.email && <p><strong>Email:</strong> {profile.email}</p>}
       <p><strong>Wallet Address:</strong> {profile.wallet}</p>
       <button onClick={onLogout}>Logout</button>
+      <button onClick={onSendTx}>Send Transaction</button>
+
+      {txReceipt && (<p>Transaction ID: <a href={`https://explorer-test.arthera.net/tx/${txReceipt.hash}`} target={'_blank'} rel="noreferrer"> {txReceipt.hash}</a></p>)}
     </div>
   );
 };
